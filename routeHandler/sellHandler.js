c32d9d5fe109bb1026ee48cc79f573d3962afdc2
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-19 20:13:13 +0600

    initialize checkOut route

--- routeHandler/sellHandler.js
+++ routeHandler/sellHandler.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const userCollection = require("../schemas/userSchemas");
+const { ObjectId } = require("mongodb");
 
 
 router.get('/', async(req, res)=>{
     const {sellerEmail , buyerId, discound, due} = req.query;
     const items = req.body;
 
+    const isAgentExist = await userCollection.findOne({_id: new ObjectId(buyerId), userType: "isAgent"});
+    if(!isAgentExist){
+        res.status(201).send({message: "please select a valid agent"})
+    }
 
-    items.forEach((item) => {
-        // comming soon
-    });
+    // items.forEach((item) => {
+    //     // comming soon
+    // });
+    res.send(isAgentExist);
 })
 
 module.exports = router;
