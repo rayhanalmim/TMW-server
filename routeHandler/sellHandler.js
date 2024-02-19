4451140754f7fe6d7216f8339415fc7c5a827731
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-19 21:50:25 +0600

    make a road map for upcomming work

--- routeHandler/sellHandler.js
+++ routeHandler/sellHandler.js
@@ -3,21 +3,30 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const userCollection = require("../schemas/userSchemas");
 const { ObjectId } = require("mongodb");
-
+const companyInfo = require("../schemas/companyCollection");
 
 router.get('/', async(req, res)=>{
-    const {sellerEmail , buyerId, discound, due} = req.query;
+    const {sellerEmail , buyerId , discount, due} = req.query;
+    const month = new Date().toISOString().substring(0, 7);
+    const year = new Date().toISOString().substring(0, 4);
     const items = req.body;
 
-    const isAgentExist = await userCollection.findOne({_id: new ObjectId(buyerId), userType: "isAgent"});
-    if(!isAgentExist){
+    const agent = await userCollection.findOne({_id: new ObjectId(buyerId), userType: "isAgent"});
+    if(!agent){
         res.status(201).send({message: "please select a valid agent"})
     }
+
+    // ---------Todo
+    // 1. productStock out
+    // 2. add price in total sell, monthly sell and yearly sell in company info
+    // 3. add purches product in agent product collection
+    // 4. add due ammount in agent collection if exists
+    // 5. push every purches product in sell collection with date
 
     // items.forEach((item) => {
     //     // comming soon
     // });
-    res.send(isAgentExist);
+    res.send(agent);
 })
 
 module.exports = router;
---
+++ schemas/companyCollection.js
@@ -0,0 +1,5 @@
+const mongoose = require("mongoose");
+
+const companyInfo = mongoose.model('companyInfo', new mongoose.Schema({}, { strict: false }));
+
+module.exports = companyInfo;
