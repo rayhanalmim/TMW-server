c0d03d4bfb0bac27e77f8f52c9fdb7725ab85e78
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-20 11:42:23 +0600

    calculate total price and write some todo

--- routeHandler/sellHandler.js
+++ routeHandler/sellHandler.js
@@ -6,7 +6,8 @@ const { ObjectId } = require("mongodb");
 const companyInfo = require("../schemas/companyCollection");
 
 router.get('/', async(req, res)=>{
-    const {sellerEmail , buyerId , discount, due} = req.query;
+    const {sellerEmail , buyerId , discount, due, totalPrice} = req.query;
+    let totalSellPrice = totalPrice;
     const month = new Date().toISOString().substring(0, 7);
     const year = new Date().toISOString().substring(0, 4);
     const items = req.body;
@@ -15,9 +16,18 @@ router.get('/', async(req, res)=>{
     if(!agent){
         res.status(201).send({message: "please select a valid agent"})
     }
+    if(discount){
+        totalSellPrice = totalSellPrice - discount;
+    }
+    if(due){
+        totalSellPrice = totalSellPrice - due;
+    }
+
 
     // ---------Todo
     // 1. productStock out
+    // 2. return error if product out of stock
+    // 2. calculate total price and minus if discount and due ammount exist
     // 2. add price in total sell, monthly sell and yearly sell in company info
     // 3. add purches product in agent product collection
     // 4. add due ammount in agent collection if exists
