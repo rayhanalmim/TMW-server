c1a2dc69177cab4d259c755e68ad4c513abea7cc
rayhanalmim <rayhanalmim1@gmail.com>
2024-02-22 14:36:42 +0600

    align some code

--- schemas/cardSchema.js
+++ schemas/cardSchema.js
@@ -1,5 +1,3 @@
 const mongoose = require("mongoose");
-
 const cardCollection = mongoose.model('cardItem', new mongoose.Schema({}, { strict: false }));
-
 module.exports = cardCollection;
--- schemas/sellCollection.js
+++ schemas/sellCollection.js
@@ -1,5 +1,3 @@
 const mongoose = require("mongoose");
-
 const sellCollection = mongoose.model('sellCollection', new mongoose.Schema({}, { strict: false }));
-
 module.exports = sellCollection;
