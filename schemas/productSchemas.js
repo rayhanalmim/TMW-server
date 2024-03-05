1595ce8cfd3d8785a17016be255e513b1c15994e
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-05 18:23:52 +0600

    remove product schema

--- schemas/productSchemas.js
+++ schemas/productSchemas.js
@@ -1,32 +1,5 @@
 const mongoose = require("mongoose");
 
-const productSchema = mongoose.Schema({
-  productName: {
-    type: String,
-  },
-  image: {
-    type: String,
-  },
-  productQuantity: {
-    type: Number,
-  },
-  productPrice: {
-    type: Number,
-  },
-  productType: {
-    type: String,
-  },
-  productDescription: {
-    type: String,
-  },
-  date: {
-    type: Date,
-    default: function () {
-      return moment().tz("Asia/Dhaka").toDate();
-    },
-  },
-});
-
 const Product = mongoose.model(
   "Product",
   new mongoose.Schema({}, { strict: false })
