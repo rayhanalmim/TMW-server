295566e74a3130f8ac66c8ae7353e7fd59fdd7bd
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-13 01:25:33 +0600

    update product card api endpoint

--- routeHandler/cardHandler.js
+++ routeHandler/cardHandler.js
@@ -3,19 +3,31 @@ const { ObjectId } = require("mongodb");
 const router = express.Router();
 const mongoose = require("mongoose");
 const cardCollection = require("../schemas/cardSchema");
+const Product = require("../schemas/productSchemas");
 
 router.post("/", async (req, res) => {
-    const { userEmail } = req.query;
-    const item = req.body;
+    const { userEmail, productId } = req.query;
+    const productQuentity = parseInt(req.query.quantity);
+
+    const product = await Product.findOne({ _id: new ObjectId(productId) });
+    if(product.productQuantity < productQuentity){
+        return res.status(200).send({ message: `${product.productName} Out of stock` });
+    }
+
+    const item = {
+        productId : productId,
+        productQuentity: productQuentity
+    };
  
     const isitemExist = await cardCollection.findOne({
         $and: [
             { user: userEmail },
-            { cardItems: { $elemMatch: { _id: item._id } } }
+            { cardItems: { $elemMatch: { productId: productId } } }
         ]
     });
+
     if (isitemExist) {
-        res.status(202).send({ message: "item already listed in database" });
+        return res.status(200).send({ message: "item already listed in database" });
     } else {
         const isCardExist = await cardCollection.findOne({ user: userEmail });
         if (isCardExist) {
@@ -25,20 +37,20 @@ router.post("/", async (req, res) => {
                     $push: { cardItems: item },
                 }
             );
-            return res.status(201).send(addItem);
+            return res.status(200).send({message: "Item added successfully"});
         } else {
             const create = await cardCollection.create({
                 user: userEmail,
                 cardItems: [item]
             })
-            return res.status(200).send(create)
+            return res.status(200).send({message: "Item added successfully"});
         }
-    }
+    } 
 });
 
 router.get("/", async (req, res) => {
-    const { user } = req.query;
-    const isCardExist = await cardCollection.findOne({ user: user });
+    const { email } = req.query;
+    const isCardExist = await cardCollection.findOne({ user: email });
     if(isCardExist){
         return res.send(isCardExist)
     }else{
@@ -56,6 +68,7 @@ router.delete('/delete', async(req, res)=>{
       console.log(remove);
     res.send(remove);
 })
+
 router.delete('/deleteAllCard', async(req, res)=>{
     const {user} = req.query;
     const remove = await cardCollection.deleteOne({ user: user });
