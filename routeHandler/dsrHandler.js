1f44838d9122da9b8b1350da38598e33dc1ff7f7
rayhanalmim <rayhanalmim1@gmail.com>
2024-03-19 12:15:18 +0600

    remove id from order status

--- routeHandler/dsrHandler.js
+++ routeHandler/dsrHandler.js
@@ -27,7 +27,7 @@ router.get("/OrderNo", async (req, res) => {
 
 router.get("/orderStatus", async (req, res) => {
     const email = req.query.email;
-    const requestedData = await dsrRequest.find({"dsrInfo.email": email},{orderNo: 1, orderDate: 1, orderTime: 1, orderStatus: 1 });
+    const requestedData = await dsrRequest.find({"dsrInfo.email": email},{orderNo: 1, orderDate: 1, orderTime: 1, orderStatus: 1, _id: 0 });
     res.send(requestedData)
 });
 
