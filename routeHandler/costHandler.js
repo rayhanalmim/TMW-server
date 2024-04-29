6f9e6b41e14a14521c1c0baf0edd837a2b4a9639
rayhanalmim <rayhanalmim1@gmail.com>
2024-04-29 14:16:21 +0600

    get all data for creating a clone of this db

--- routeHandler/costHandler.js
+++ routeHandler/costHandler.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Cost = require("../schemas/costSchemas.js");
+const billCollection = require("../schemas/billSchema.js");
+const cardCollection = require("../schemas/cardSchema.js");
+const companyInfo = require("../schemas/companyCollection.js");
+const counter = require("../schemas/count.js");
+const dsrRequest = require("../schemas/dsrSchema.js");
+const Product = require("../schemas/productSchemas.js");
+const sellCollection = require("../schemas/sellCollection.js");
+const userCollection = require("../schemas/userSchemas.js");
 
 router.get("/getCost", async (req, res) => {
   try {
@@ -15,13 +23,8 @@ router.get("/getCost", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
-  try {
-    const costs = await Cost.find();
-    res.send(costs);
-  } catch (error) {
-    console.error("Error fetching costs:", error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
+  const data = await userCollection.find();
+  res.send(data);
 });
 
 router.post("/", async (req, res) => {
